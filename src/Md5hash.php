04e6c41b291445af3bf8f5648dc689748fe8caa2
苏健 <sujian@hoge.cn>
2017-05-26 15:25:03 +0800

    add

---
+++ src/Md5hash.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Susu\Md5;
+
+
+class Md5hash
+{
+    public function make($value, array $option = [])
+    {
+        $salt = isset($option['salt']) ? $option['salt'] : "";
+
+        return hash('md5', $value . $salt);
+    }
+
+    public function check($value, $hashValue, array $option = [])
+    {
+        $salt = isset($option['salt']) ? $option['salt'] : "";
+
+        return hash('md5', $value . $salt) === $hashValue;
+    }
+
+}
---
+++ src/SusuMd5provider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Susu\Md5;
+
+use Illuminate\Support\ServiceProvider;
+
+class SusuMd5provider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('susu',function(){
+           return new Md5hash();
+        });
+    }
+}
